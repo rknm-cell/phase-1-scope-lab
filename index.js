b508f671b7c97ccd930ca4730518cf96c52c5756
rknm-cell <racknahm@gmail.com>
2023-03-21 09:24:52 -0400

    completed lab tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"
+var bestCustomer
+const leastFavoriteCustomer = "tim"
+function setBestCustomer() {
+    return bestCustomer = "not bob";
+};
+//function upperCaseCustomerName(customerName) {
+//    return customerName.toUpperCase();
+//}
+//        why doesnt this work???
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+};
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "probably bob"
+    return leastFavoriteCustomer;
+}
